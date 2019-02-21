7e67adc0870e0a1c557ec506ab70de59be952779
Grumpy Pixel <grumpypixel@protonmail.com>
2019-02-21 22:07:59 +0100

    Update game input manager

--- Assets/Scripts/Game/GameInput.cs
+++ Assets/Scripts/Game/GameInput.cs
@@ -5,8 +5,11 @@ namespace game
 {
 	public class GameInput : MonoBehaviour
 	{
-		private IMessageDispatcher	m_messageDispatcher;
-		private int					m_fingerId = -1;
+		private IMessageDispatcher m_messageDispatcher;
+		private int m_fingerId = -1;
+
+		private Vector2 m_lastPointerPosition;
+		private float   m_lastPointerTime;
 
 		void Start()
 		{
@@ -17,7 +20,7 @@ namespace game
 		{
 			if (m_fingerId == -1)
 			{
-#if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IOS)
+		#if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IOS)
 				if (Input.touchCount > 0)
 				{
 					Touch touch = Input.GetTouch(0);
@@ -27,17 +30,19 @@ namespace game
 						SendTouchBeganMessage(touch.position, m_fingerId);
 					}
 				}
-#else
+		#else
 				if (Input.GetMouseButtonDown(0))
 				{
 					m_fingerId = 0;
-					SendTouchBeganMessage(Input.mousePosition, m_fingerId);
+					m_lastPointerPosition = Input.mousePosition;
+					m_lastPointerTime = Time.time;
+					SendTouchBeganMessage(m_lastPointerPosition, m_fingerId);
 				}
-#endif
+		#endif
 			}
 			else
 			{
-#if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IOS)
+		#if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IOS)
 				int touchCount = Input.touchCount;
 				for (int i = 0; i < touchCount; ++i)
 				{
@@ -46,27 +51,51 @@ namespace game
 					{
 						if (touch.phase == TouchPhase.Moved)
 						{
-							SendTouchMovedMessage(touch.position);
+							SendTouchMovedMessage(touch.position, touch.deltaPosition, touch.deltaTime);
 						}
 						else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
 						{
 							m_fingerId = -1;
-							SendTouchEndedMessage(touch.position, touch.phase == TouchPhase.Canceled);
+							bool canceled = touch.phase == TouchPhase.Canceled;
+							SendTouchEndedMessage(touch.position, touch.deltaPosition, touch.deltaTime, canceled);
 						}
 					}
 				}
-#else
+		#else
 				if (m_fingerId == 0)
 				{
-					SendTouchMovedMessage(Input.mousePosition);
+					Vector2 position = Input.mousePosition;
+					float now = Time.time;
+
+					Vector2 deltaPosition = position - m_lastPointerPosition;
+					float deltaTime = now - m_lastPointerTime;
+					SendTouchMovedMessage(position, deltaPosition, deltaTime);
+
+					m_lastPointerPosition = position;
+					m_lastPointerTime = now;
 				}
 				if (Input.GetMouseButtonUp(0))
 				{
 					m_fingerId = -1;
-					SendTouchEndedMessage(Input.mousePosition, false);
+					Vector2 position = Input.mousePosition;
+					float now = Time.time;
+
+					Vector2 deltaPosition = position - m_lastPointerPosition;
+					float deltaTime = now - m_lastPointerTime;
+					SendTouchEndedMessage(position, deltaPosition, deltaTime, false);
+
+					m_lastPointerPosition = position;
+					m_lastPointerTime = now;
 				}
-#endif
+		#endif
 			}
+
+		#if UNITY_ANDROID
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				m_messageDispatcher.AddMessage<EscapePressedMessage>();
+			}
+		#endif
 		}
 
 		private void SendTouchBeganMessage(Vector2 position, int fingerId)
@@ -76,27 +105,31 @@ namespace game
 			message.isPointerOverUIObject = IsPointerOverGameObject(fingerId);
 		}
 
-		private void SendTouchMovedMessage(Vector2 position)
+		private void SendTouchMovedMessage(Vector2 position, Vector2 deltaPosition, float deltaTime)
 		{
 			TouchMovedMessage message = m_messageDispatcher.AddMessage<TouchMovedMessage>();
 			message.touchPosition = position;
+			message.deltaPosition = deltaPosition;
+			message.deltaTime = deltaTime;
 		}
 
-		private void SendTouchEndedMessage(Vector2 position, bool canceled)
+		private void SendTouchEndedMessage(Vector2 position, Vector2 deltaPosition, float deltaTime, bool canceled)
 		{
 			TouchEndedMessage message = m_messageDispatcher.AddMessage<TouchEndedMessage>();
 			message.touchPosition = position;
+			message.deltaPosition = deltaPosition;
+			message.deltaTime = deltaTime;
 			message.canceled = canceled;
 		}
 
 		private bool IsPointerOverGameObject(int fingerId)
 		{
 			EventSystem eventSystem = EventSystem.current;
-#if UNITY_EDITOR
+		#if UNITY_EDITOR
 			return (eventSystem.IsPointerOverGameObject() && eventSystem.currentSelectedGameObject != null);
-#else
+		#else
 			return (eventSystem.IsPointerOverGameObject(fingerId) && eventSystem.currentSelectedGameObject != null);
-#endif
+		#endif
 		}
 	}
 }
--- Assets/Scripts/Game/Messages.cs
+++ Assets/Scripts/Game/Messages.cs
@@ -47,22 +47,32 @@ namespace game
 	public class TouchMovedMessage : Message
 	{
 		public Vector2 touchPosition;
+		public Vector2 deltaPosition;
+		public float deltaTime;
 
 		public override void Reset()
 		{
 			touchPosition = Vector2.zero;
+			deltaPosition = Vector2.zero;
+			deltaTime = 0f;
 		}
 	}
 
 	public class TouchEndedMessage : Message
 	{
 		public Vector2 touchPosition;
+		public Vector2 deltaPosition;
+		public float deltaTime;
 		public bool canceled;
 
 		public override void Reset()
 		{
 			touchPosition = Vector2.zero;
+			deltaPosition = Vector2.zero;
+			deltaTime = 0f;
 			canceled = false;
 		}
 	}
+
+	public class EscapePressedMessage : Message {}
 }
