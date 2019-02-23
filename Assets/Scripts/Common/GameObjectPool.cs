07ccecb2f363137eb03315e82f66263475aa9dde
Grumpy Pixel <grumpypixel@protonmail.com>
2019-02-23 13:14:20 +0100

    Update according to boilerplate changes

--- Assets/Scripts/Common/GameObjectPool.cs
+++ Assets/Scripts/Common/GameObjectPool.cs
@@ -3,6 +3,24 @@ using UnityEngine;
 
 namespace game
 {
+	public class PoolObject : MonoBehaviour
+	{
+		private bool m_isAvailableForReuse = true;
+
+		public bool isAvailableForReuse
+		{
+			get
+			{
+				return m_isAvailableForReuse && this.gameObject.activeInHierarchy == false;
+			}
+			set
+			{
+				m_isAvailableForReuse = value;
+				this.gameObject.SetActive(!value);
+			}
+		}
+	}
+
 	public class GameObjectPool : MonoBehaviour
 	{
 		[System.Serializable]
